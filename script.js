9ec498533b313564a97ee7d6f3c873256b50636b
demian.juarez <demian.juarez@gm2dev.com>
2023-08-31 12:59:27 -0300

    doing css

--- script.js
+++ script.js
@@ -1,15 +1,113 @@
-const products = []
-
-const crearProduct = (name, price, id) => {
-    return {
-        itemPhoto: "llaveTubo.png",
-        itemName : name,
-        itemPrice : price,
-        itemId : id
+const products = [
+    //FeaturedProducts
+    {
+        id: "featured-0",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Featured",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "featured-1",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Featured",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "featured-2",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Featured",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "featured-3",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Featured",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "ofert-4",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Ofert",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "ofert-5",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Ofert",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "ofert-6",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Ofert",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "ofert-7",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "Ofert",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "news-8",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "News",
+        precio: 999,
+        discountPrecio: 699
+    },
+    {
+        id: "news-9",
+        img: ".src/llaveTubo.png",
+        title: "Lorem Ipsum Dolor Sit Amet Consectetur",
+        categoria: "News",
+        precio: 999,
+        discountPrecio: 699
     }
+]
+
+const productsContainer = document.querySelector("#products")
+
+const uploadFeaturedProducts = () => {
+    products.forEach(product => {
+        const div = document.createElement("div")
+        div.classList.add("featuredProduct");
+        div.innerHTML = `
+            <div class="info">
+                <img src="${product.img}" alt="${product.title}">
+                <p>${product.title}<p>
+                <p>${product.id}</p>
+            </div>
+            <div class="info2">
+                <div class="buyButton">
+                    <img src="src/shopping-bag.svg" alt="bag">
+                    <img src="src/plus.svg" alt="plus">
+                    </div>
+                    <div class="price">
+                        <p>$${product.precio}</p>
+                        <p>$${product.discountPrecio}</p>
+                    </div>
+                </div>
+        `;
+
+        productsContainer.append("div")
+    })
 }
 
-const llaveDeTubo = crearProduct("llave de Tubo", 120, 1)
-products.push(llaveDeTubo)
-
-console.log(products)
+uploadFeaturedProducts()
